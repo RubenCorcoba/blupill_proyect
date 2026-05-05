ea98e84434ac05142d0b8e3bb53be2f3aeaa9493
Fernando Alberto Miranda Bonomi <83925496+fmirandabonomi@users.noreply.github.com>
2026-05-05 17:35:22 -0300

    Dos canales. Copia privada de librería Ethernet para mantener modificación uso DMA

--- include/bsp.hpp
+++ include/bsp.hpp
@@ -3,16 +3,30 @@
 #include <stdint.h>
 
 // Definiciones de tamaño de buffer
-constexpr int NMUESTRAS_BUFFER = 512; // Número de muestras por cada mitad del buffer (cada muestra = 2 bytes)
+
+// Cantidad de bytes por muestra (cada muestra son dos canales y cada canal 16
+// bit)
+constexpr int NCANALES_ADC = 2;
+constexpr int NBYTES_CANAL = 2;
+constexpr int NBYTES_MUESTRA = NCANALES_ADC * NBYTES_CANAL;
+
+// Número de muestras por cada mitad del
+// buffer (cada muestra = NBYTES_MUESTRA bytes)
+constexpr int NMUESTRAS_BUFFER = 512;
 
 // Buffers y variables de control
-extern uint8_t buffer_ADC[2][NMUESTRAS_BUFFER * 2]; // Doble buffer circular
-extern volatile uint32_t cuenta_buffers_cargados; // Contador de buffers llenados por el DMA
-extern volatile uint32_t cuenta_buffers_vistos;   // Contador de buffers procesados en el bucle principal
+extern uint8_t
+    buffer_ADC[2][NMUESTRAS_BUFFER * NBYTES_MUESTRA];  // Doble buffer circular
+extern volatile uint32_t
+    cuenta_buffers_cargados;  // Contador de buffers llenados por el DMA
+extern volatile uint32_t
+    cuenta_buffers_vistos;  // Contador de buffers procesados en el bucle
+                            // principal
 
 // Declaración de funciones
 void bsp_init();
-void transmite(uint8_t* datos, int nbytes); // Función para transmitir datos al servidor
+void transmite(uint8_t* datos,
+               int nbytes);  // Función para transmitir datos al servidor
 
 // Prototipo de la interrupción DMA (declaración externa)
 extern "C" void DMA1_Channel1_IRQHandler(void);
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
-/// Versión 13 bluepill_w5100_ethernet RDC - Remapeo SPI1 a pines alternativos (PA15, PB3, PB4, PB5)
+/// Versión 13 bluepill_w5100_ethernet RDC - Remapeo SPI1 a pines alternativos
+/// (PA15, PB3, PB4, PB5)
 #include <Arduino.h>
+
 #include "bsp.hpp"
 ////////////////////////////////////////////////////////////////
-
 
 ////////////////////////////////////////////////////////////////
 // Función setup: Configuración inicial
@@ -11,24 +12,23 @@ void setup(void) {
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, 1);
 
-    pinMode(PB9, OUTPUT); 
+    pinMode(PB9, OUTPUT);
     digitalWrite(PB9, LOW);
 }
-
-
 
 ////////////////////////////////////////////////////////////////
 // Función loop: Ciclo principal del programa
 void loop(void) {
     // Verificar si hay un buffer listo para transmitir
     if (cuenta_buffers_cargados == cuenta_buffers_vistos) {
-        return; // No hay nuevos datos, salir
+        return;  // No hay nuevos datos, salir
     }
 
     // Si hay un nuevo buffer disponible
     if (cuenta_buffers_cargados == cuenta_buffers_vistos + 1) {
-        transmite(buffer_ADC[cuenta_buffers_vistos % 2], NMUESTRAS_BUFFER * 2); // Enviar datos
-        cuenta_buffers_vistos++; // Registrar que este buffer ya fue procesado
+        transmite(buffer_ADC[cuenta_buffers_vistos % 2],
+                  NMUESTRAS_BUFFER * NBYTES_MUESTRA);  // Enviar datos
+        cuenta_buffers_vistos++;  // Registrar que este buffer ya fue procesado
     } else {
         // sincronizar para evitar desbordes
         cuenta_buffers_vistos = cuenta_buffers_cargados;
